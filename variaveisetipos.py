83ec4b57bfa8549e8fb35a7d94e5c1af8f435e42
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-06-23 11:40:07 -0300

    ex.1

---
+++ variaveisetipos.py
@@ -0,0 +1,13 @@
+name = 'Carolina'
+surname ='De Oliveira'
+age =26
+birth_year = 2023 - age
+legal_age = age >=18
+height_meters = 1.67
+
+print('Name:', name)
+print('Surname:', surname)
+print('Age:', age)
+print('Birth Year:', birth_year)
+print('Legal of age:', legal_age)
+print('Height:', height_meters)
